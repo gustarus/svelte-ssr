1a50a922d1dcf6bca8f0ec37d55d3845568e9e81
Pavel <gustarus@gmail.com>
2020-04-30 11:24:13 +0300

    repair exports

--- build/server.d.ts
+++ build/server.d.ts
@@ -1,5 +1,5 @@
-import createServer from './helpers/createServer';
 import createRenderMiddleware from './helpers/createRenderMiddleware';
+import createServer from './helpers/createServer';
 import createStaticMiddleware from './helpers/createStaticMiddleware';
 import resolveDesiredPort from './helpers/resolveDesiredPort';
 export { createServer, createRenderMiddleware, createStaticMiddleware, resolveDesiredPort };
--- build/server.js
+++ build/server.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const createServer_1 = __importDefault(require("./helpers/createServer"));
-exports.createServer = createServer_1.default;
 const createRenderMiddleware_1 = __importDefault(require("./helpers/createRenderMiddleware"));
 exports.createRenderMiddleware = createRenderMiddleware_1.default;
+const createServer_1 = __importDefault(require("./helpers/createServer"));
+exports.createServer = createServer_1.default;
 const createStaticMiddleware_1 = __importDefault(require("./helpers/createStaticMiddleware"));
 exports.createStaticMiddleware = createStaticMiddleware_1.default;
 const resolveDesiredPort_1 = __importDefault(require("./helpers/resolveDesiredPort"));
